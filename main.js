c8c0434dca0bdbe08c0e8b707a341ff7bf155480
Artem-1-1 <aleksejchukartem@gmail.com>
2025-10-27 13:04:11 +0300

    Completed create grid,added style to button and comleted extra-credit(1 of 2)

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-const { createElement } = require("react");
-
 const container = document.getElementById('main-container');
-const myInput = document.getElementById('inputNum');
 const btn = document.getElementById('btn');
-btn.addEventListener('click', function() {
-    const inputValue = myInput.value;
-    console.log('Вы ввели ' + inputValue)
-})
 
-for (let i = 0;i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square')
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = 'red';
-    })
-    container.appendChild(square);
+function randomRGB() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
 }
+function drawGrid(num) {
+    container.textContent = "";
+    const squareSize = 500 / num;
+    for (i = 1; i <= num * num; i++) {
+        const square = document.createElement('square');
+        square.classList.add('square');
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = randomRGB();
+        })
+
+    container.appendChild(square);
+    }
+}
+drawGrid(16);
+
+btn.addEventListener('click', function() {
+    let number = parseInt(prompt('Enter a number squares in grid.'))
+    if (number > 0 && number <= 100) {
+        drawGrid(number);
+    }
+    else {
+        alert('Pease type a number greater than 0 and below 100.')
+    }
+});
+
+
+
